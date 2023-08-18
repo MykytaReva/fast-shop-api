376601f049d11313cc713634383fbe8c6b631db1
revam <revam@ryanair.com>
2023-08-19 00:29:47 +0200

    activation email

---
+++ shop/smtp_email.py
@@ -0,0 +1,47 @@
+import os
+
+from dotenv import load_dotenv
+from jose import jwt
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+from shop import settings
+from shop.database import SessionLocal
+from shop.models import User
+
+load_dotenv()
+
+
+def send_activation_email(user_id: int, db: SessionLocal):
+    try:
+        # Get your SendGrid API key from environment variables
+        sendgrid_api_key = os.environ.get("SENDGRID_API_KEY")
+
+        if not sendgrid_api_key:
+            raise Exception("SendGrid API key is missing")
+
+        # Create a SendGrid client
+        sg = SendGridAPIClient(sendgrid_api_key)
+
+        user_email = db.query(User).filter(User.id == user_id).first().email
+        subject = "Welcome to our shop!"
+        token = jwt.encode({"sub": str(user_id)}, settings.JWT_SECRET, algorithm=settings.ALGORITHM)
+        html_content = (
+            "You have successfully registered to our shop."
+            f" Please click <a href='http://127.0.0.1:8000/verification/?token={token}'>here</a>"
+            " to activate your account."
+        )
+        # Create a Mail object
+        message = Mail(
+            from_email=os.environ.get("FROM_EMAIL"), to_emails=user_email, subject=subject, html_content=html_content
+        )
+
+        # Send the email
+        response = sg.send(message)
+
+        if response.status_code == 202:
+            return {"message": "Email sent successfully."}
+        else:
+            return {"message": "Failed to send email."}
+    except Exception as e:
+        print("An error occurred:", str(e))
