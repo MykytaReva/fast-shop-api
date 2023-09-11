193fa44a39db2cb6d54850218259ddc7c9c48fbc
revam <revam@ryanair.com>
2023-09-11 20:14:07 +0200

    docker-compose

--- shop/database.py
+++ shop/database.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./shop.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@db_postgres/shop-online-api"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./shop.db"
 SQLALCHEMY_DATABASE_URL_TEST = "sqlite:///./test.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 test_engine = create_engine(SQLALCHEMY_DATABASE_URL_TEST, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
